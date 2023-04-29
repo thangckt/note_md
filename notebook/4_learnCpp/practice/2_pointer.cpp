6c722c828e245f855b1bf7f1e0d0a4fbe549263d
thangckt <46436648+thangckt@users.noreply.github.com>
2023-04-29 21:03:42 +0900

    u

--- notebook/4_learnCpp/practice/2_pointer.cpp
+++ notebook/4_learnCpp/practice/2_pointer.cpp
@@ -1,57 +1,22 @@
-
 #include <stdio.h>
 int main()
 {
-  // Khai báo + khởi tạo biến value = 10
-  int value = 10;
+   int* pc, c;
 
-  // Lấy giá trị của biến value
-  printf("\nGia tri cua `value` = %d", value);
-  // Lấy địa chỉ của biến value
-  printf("\nDia tri cua `value` = %d", &value);
+   c = 22;
+   printf("Address of c: %p\n", &c);
+   printf("Value of c: %d\n\n", c);  // 22
 
-  printf("\n-------------------\n");
+   pc = &c;
+   printf("Address of pointer pc: %p\n", pc);
+   printf("Content of pointer pc: %d\n\n", *pc); // 22
 
-  /*
-  Khai báo + khởi tạo biến con trỏ p
-  có giá trị là địa chỉ của biến value
-  */
-  int *p = &value;
+   c = 11;
+   printf("Address of pointer pc: %p\n", pc);
+   printf("Content of pointer pc: %d\n\n", *pc); // 11
 
-  // Lấy giá trị của con trỏ p
-  printf("\nGia tri cua con tro `p` = %d", p);
-  // Lấy địa chỉ của con trỏ p
-  printf("\nDia tri cua con tro `p` = %d", &p);
-  // Lấy giá trị của biến ma con trỏ p đang trỏ tới dùng toán tử *
-  printf("\nGia tri cua bien ma con tro `p` dang tro toi = %d", *p);
-
-  printf("\n-------------------\n");
-
-  /*
-  Thay đổi giá trị của biến value thông qua con trỏ p
-  Giống như hàm scanf() có thể thay đổi giá trị của biến khi nhận vào địa chỉ,
-  con trỏ khi có địa chỉ của 1 biến hoàn toàn có thể thay đổi giá trị của
-  biến đó theo cách dưới đây:
-  */
-  // Lấy giá trị của biến value
-  printf("\nGia tri cua `value` = %d", value);
-  // Thay đổi giá trị của biến value thông qua `p`
-  *p = 100;
-  // Lấy giá trị của biến value
-  printf("\nGia tri cua `value` = %d", value);
-  // Lấy giá trị của biến ma con trỏ p đang trỏ tới dùng toán tử *
-  printf("\nGia tri cua bien ma con tro `p` dang tro toi = %d", *p);
-
-  printf("\n-------------------\n");
-
-  /*
-  Việc lấy giá trị của biến thông qua con trỏ
-  chỉ là 1 cách khác để lấy được giá trị của biến đó.
-
-  */
-  value = 1000;
-  // Lấy giá trị của biến value
-  printf("\nGia tri cua `value` = %d", value);
-  // Lấy giá trị của biến ma con trỏ p đang trỏ tới dùng toán tử *
-  printf("\nGia tri cua bien ma con tro `p` dang tro toi = %d", *p);
+   *pc = 2;
+   printf("Address of c: %p\n", &c);
+   printf("Value of c: %d\n\n", c); // 2
+   return 0;
 }
